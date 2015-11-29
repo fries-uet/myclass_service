6160229ca711f7931345f9e9fa8a91024677317f
Tu TV <tutv95@gmail.com>
2015-11-30 05:14:40 +0700

    Updated assets

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,14 +3,22 @@
 namespace App\Http\Controllers;
 
 use FriesMail;
+use Hash;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Session;
 
 class TestController extends Controller {
 	public function test_helper() {
-		$friesMap = new FriesMail( 'Đây là title', 'Đây là nội dung' );
-		$friesMap->setFromName( 'Fries Team' )->setFrom( 'fries.uet@gmail.com' )->addTo( 'tutv95@gmail.com' )
-		         ->sendMail();
+//		$friesMap = new FriesMail( 'Đây là title', 'Đây là nội dung' );
+//		$friesMap->setFromName( 'Fries Team' )->setFrom( 'fries.uet@gmail.com' )->addTo( 'tutv95@gmail.com' )
+//		         ->sendMail();
+
+		$a = bcrypt( 'aaa' );
+
+		Session::put( 'key', "{$a}" );
+
+		dd( session( 'key' ) );
 	}
 }
