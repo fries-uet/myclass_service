36a7d360d239d38394b7baef577cab95fac7c9eb
Tu TV <tutv95@gmail.com>
2015-12-29 18:57:12 +0700

    __

---
+++ resources/views/partials/list_result.blade.php
@@ -0,0 +1,11 @@
+<div class="results">
+    <table class="table">
+        <thead>
+        <tr>
+            <th>STT</th>
+            <th>Tên MH</th>
+            <th>Mã MH</th>
+        </tr>
+        </thead>
+    </table>
+</div>
