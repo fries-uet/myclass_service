a250ae880e4579bb999d3ad5bb3d6ee8d2aba2ef
Tu TV <tutv95@gmail.com>
2016-01-06 23:43:53 +0700

    -_

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -4,23 +4,25 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
-class VerifyCsrfToken extends BaseVerifier {
-	/**
-	 * The URIs that should be excluded from CSRF verification.
-	 *
-	 * @var array
-	 */
-	protected $except
-		= [
-			'v1/login',
-			'v1/register',
-			'v1/update',
-			'v1/getGroup',
-			'v1/post',
-			'v1/post/comment',
-			'v1/getPosts',
-			'v1/timetable',
-			'v1/like',
-			'v1/vote',
-		];
+class VerifyCsrfToken extends BaseVerifier
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except
+        = [
+            'v1/login',
+            'v1/register',
+            'v1/update',
+            'v1/getGroup',
+            'v1/post',
+            'v1/post/comment',
+            'v1/getPosts',
+            'v1/timetable',
+            'v1/like',
+            'v1/vote',
+            'gcm/register',
+        ];
 }
