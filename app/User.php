f668d5aea2a7572a5db0b0257d02937cf9a8d4ab
Tu TV <tutv95@gmail.com>
2016-02-18 00:33:14 +0700

    Test

--- app/User.php
+++ app/User.php
@@ -55,7 +55,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     public static function getInfoById($id)
     {
         var_dump($id);
-        $users = User::all()->where('id', 173);
+        $users = User::all()->where('id', '173');
 
         var_dump($users);
         die();
