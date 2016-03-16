02d0d38961e01063a3e79445007cdad8df8d68b9
Tu TV <tutv95@gmail.com>
2016-03-17 02:03:01 +0700

    Updated

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,36 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Comment;
 use App\Post;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use DB;
 
 use App\Http\Requests;
 use stdClass;
 
-class CommentController extends Controller {
-	public function comment( Request $request ) {
-		onlyAllowPostRequest( $request );
+class CommentController extends Controller
+{
+    public function comment(Request $request)
+    {
+        onlyAllowPostRequest($request);
 
-		$all = $request->only( [
-			'post',
-			'author',
-			'content',
-		] );
+        $all = $request->only([
+            'post',
+            'author',
+            'content',
+        ]);
 
-		$comment = Comment::create( [
-			'post'    => intval( $all['post'] ),
-			'author'  => intval( $all['author'] ),
-			'content' => $all['content'],
-		] );
+        $post_id = intval($all['post']);
+        $author_id = intval($all['author']);
+        $content = $all['content'];
 
-		$c = Comment::getCommentInfoById( $comment->id );
+        $comment = Comment::create([
+            'post' => $post_id,
+            'author' => $author_id,
+            'content' => $content,
+        ]);
 
-		/**
-		 * Dữ liệu trả về
-		 */
-		$response          = new stdClass();
-		$response->error   = false;
-		$response->comment = $c;
+        $post = DB::table('posts')->where('id', $post_id)->update([
+            'updated_at' => Carbon::now(),
+        ]);
 
-		return response()->json( $response );
-	}
+        $c = Comment::getCommentInfoById($comment->id);
+
+        /**
+         * Dữ liệu trả về
+         */
+        $response = new stdClass();
+        $response->error = false;
+        $response->comment = $c;
+
+        return response()->json($response);
+    }
 }
