1068c165495ffff10a5f64f16107e81005d64942
Tu TV <tutv95@gmail.com>
2016-03-17 14:45:46 +0700

    Updated

--- app/Post.php
+++ app/Post.php
@@ -2,41 +2,46 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use stdClass;
 
-class Post extends Model {
-	protected $table = 'posts';
-	protected $fillable
-		= [
-			'title',
-			'content',
-			'author',
-			'group',
-			'isIncognito',
-			'base',
-			'type',
-		];
+class Post extends Model
+{
+    protected $table = 'posts';
+    protected $fillable
+        = [
+            'title',
+            'content',
+            'author',
+            'group',
+            'isIncognito',
+            'base',
+            'type',
+        ];
 
-	public static function getPostInfoById( $id ) {
-		$posts = Post::all()->where( 'id', intval( $id ) );
-		if ( $posts->count() == 0 ) {
-			return null;
-		}
+    public static function getPostInfoById($id)
+    {
+        $posts = Post::all()->where('id', intval($id));
+        if ($posts->count() == 0) {
+            return null;
+        }
 
-		$post           = $posts->first();
-		$p              = new stdClass();
-		$p->id          = $post->id;
-		$p->title       = $post->title;
-		$p->content     = $post->content;
-		$p->group       = $post->group;
-		$p->like        = intval( $post->like );
-		$p->author      = User::getInfoById( $post->author );
-		$p->isIncognito = boolval( $post->isIncognito );
-		$p->type        = $post->type;
-		$p->base        = $post->base;
-		$p->comments    = Comment::getCommentsByPostId( $post->id );
+        $post = $posts->first();
+        $p = new stdClass();
+        $p->id = $post->id;
+        $p->title = $post->title;
+        $p->content = $post->content;
+        $p->group = $post->group;
+        $p->like = intval($post->like);
+        $p->author = User::getInfoById($post->author);
+        $p->isIncognito = boolval($post->isIncognito);
+        $p->type = $post->type;
+        $p->base = $post->base;
+        $date = new Carbon($p->created_at);
+        $p->created_at = $date->format('d/m/Y');
+        $p->comments = Comment::getCommentsByPostId($post->id);
 
-		return $p;
-	}
+        return $p;
+    }
 }
