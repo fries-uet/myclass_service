565acbaec16fe313289191b1f5e01a30c1fe8cd7
Tu TV <tutv95@gmail.com>
2016-03-19 02:35:16 +0700

    Updated dislike

--- resources/views/api/recapcha.blade.php
+++ resources/views/api/recapcha.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     <div class="row">
         <div class="col-md-6">
-            <div class="g-recaptcha" data-sitekey="6Len-xMTAAAAAGjzru62z_yyKzQEpRz-5SnUJLTg"></div>
+            <div class="g-recaptcha" data-sitekey="6Ld4MRsTAAAAAOdwBj2DAAXk2e3oem3zkAFWJBUs"></div>
         </div>
     </div>
 </div>
